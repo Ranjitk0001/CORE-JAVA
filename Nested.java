9ede94ebbee00d18cd8c6384b17f9cbf34c2344f
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2024-01-24 10:44:13 +0530

    Nested.java
    class Outer in  class  Inner   is Nested class

---
+++ Nested.java
@@ -0,0 +1,41 @@
+// Neasted 
+class Outer
+{
+  int a=100;
+  Inner obj =new Inner();
+
+  void access()
+  {
+   Inner i=new Inner();
+   i.showa();
+  }
+
+  class Inner
+  {
+   void showa()
+    {
+     System.out.println("a="+a);
+    }
+  }
+   
+}
+
+class Nested
+{
+ public static void main(String args[])
+ {
+  Outer o=new Outer();
+  o.access();
+
+  o.obj.showa();
+
+  Outer.Inner oi=new Outer().new Inner();
+  oi.showa();
+
+  oi=o.obj;
+  oi.showa();
+ 
+  new Outer().new Inner().showa();
+ }
+ 
+}
