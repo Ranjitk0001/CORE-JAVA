ad84678f472c7df6ee6f85a73cfa53e51e3df011
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2024-01-30 11:19:52 +0530

    Update Super.java

--- Super.java
+++ Super.java
@@ -28,3 +28,53 @@ class Super
     
  }
 }
+
+
+/*
+
+//Super.java
+class Box 
+{
+ double height,length,width;
+  Box(double w,double h,double l)
+  {
+   width=w;
+   height=h;
+   length=l;
+  }
+  
+  void valume()
+  {
+   System.out.println("Valume=" +width*height*length);
+  }
+}
+
+class Mybox extends Box
+{
+double density;
+  Mybox(double w,double h,double l, double d)
+  {
+   super(w,h,l);
+   density=d;
+  }
+ 
+  void mass()
+  {
+    System.out.println("Mass=" +width*height*length*density);
+  }
+}
+
+class Super 
+{
+ public static void main(String args[])
+ {
+  Mybox m=new Mybox(10,10,10,5);
+  m.valume();
+  m.mass();
+  
+  
+ }
+}
+
+
+*/
